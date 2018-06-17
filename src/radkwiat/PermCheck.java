c44cee05a5c1a9c6312380ebf0b1279f89b377bc
Radosław Kwiatkowski <35296705+radkwiat84@users.noreply.github.com>
2018-06-17 22:52:30 +0200

    added PermCheck

---
+++ src/radkwiat/PermCheck.java
@@ -0,0 +1,27 @@
+package radkwiat;
+
+import java.util.Arrays;
+
+public class PermCheck {
+
+	public static void main(String[] args) {
+
+		int[] A = { 4, 1, 3 };
+		System.out.println(solution(A));
+
+	}
+
+	public static int solution(int[] A) {
+		Arrays.sort(A);
+		if (A[0] != 1)
+			return 0;
+
+		for (int i = 0; i < A.length - 1; i++) {
+			if (A[i] + 1 != A[i + 1])
+				return 0;
+		}
+
+		return 1;
+	}
+
+}
